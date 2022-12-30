3183f9e25f5da9f7b2cb2d050c9c672690ff1c17
KomuRui <103985106+KomuRui@users.noreply.github.com>
2022-12-30 23:26:00 +0900

    コード修正

--- プロジェクトファイル/Mob/MainMob.cpp
+++ プロジェクトファイル/Mob/MainMob.cpp
@@ -4,9 +4,15 @@
 //定数
 namespace
 {
+	////////////////////////////アニメーション////////////////////////////
+
 	static const int START_FRAME = 0;	  //開始フレーム
 	static const int END_FRAME = 120;     //終了フレーム
 	static const float ANIM_SPEED = 1.0f; //アニメーションの再生速度
+
+	////////////////////////////その他////////////////////////////
+
+	static const float PLAYER_LOOK_DISTANCE = 8; //Playerの方を向く時の距離
 }
 
 //コンストラクタ
@@ -42,7 +48,7 @@ void MainMob::LookPlayer()
 		transform_.mmRotate_ = XMMatrixInverse(nullptr, toPlayer_);
 
 	//Playerとの距離が8以内ならPlayerの方を向く
-	if (RangeCalculation(transform_.position_, GameManager::GetpPlayer()->GetPosition()) < 8)
+	if (RangeCalculation(transform_.position_, GameManager::GetpPlayer()->GetPosition()) < PLAYER_LOOK_DISTANCE)
 	{
 		toPlayer_ = XMMatrixLookAtLH(XMVectorSet(ZERO, ZERO, ZERO, ZERO), XMLoadFloat3(new XMFLOAT3(GameManager::GetpPlayer()->GetPosition())) - XMLoadFloat3(&transform_.position_), up_);
 		transform_.mmRotate_ = XMMatrixInverse(nullptr, toPlayer_);
