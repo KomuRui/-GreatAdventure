0775ce1229b897e0d633d70e68005c22f59946e9
rui1106 <210465@jc-21.jp>
2023-01-10 14:29:33 +0900

    CSV追加

--- プロジェクトファイル/Engine/CsvReader.h
+++ プロジェクトファイル/Engine/CsvReader.h
@@ -1,53 +1,68 @@
 #pragma once
-#include <vector>
 #include <string>
+#include <vector>
 
-//-----------------------------------------------------------
-//CSVファイルを扱うクラス
-//-----------------------------------------------------------
-class CsvReader
-{
-	//読み込んだデータを入れておく2次元配列
-	std::vector<std::vector<std::string>> data_;
+/// <summary>
+/// 外部から情報を持ってくるクラス
+/// </summary>
+class CsvReader {
 
-	//「,」か「改行」までの文字列を取得
-	//引数：reuult	結果を入れるアドレス
-	//引数：data	もとの文字列データ
-	//引数：index	何文字目から調べるか
-	//戻値：なし
-	void GetToComma(std::string *result, std::string data, DWORD* index);
+	//構造体(一行)
+	struct LINEREC {
+		std::vector<std::string> record;
+	};
+
+	//全ての文字格納用
+	std::vector<LINEREC> all;
 
 public:
-	//コンストラクタ
-	CsvReader();
 
-	//デストラクタ
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	/// <param name="filename">ファイルの名前</param>
+	CsvReader(std::string filename);
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
 	~CsvReader();
 
-	//CSVファイルのロード
-	//引数：fileName　ファイル名
-	//戻値：成功→true　失敗→false
-	bool Load(std::string fileName);
+	/// <summary>
+	/// 読み込んだファイルが何行あるか
+	/// </summary>
+	/// <returns>総行数</returns>
+	int GetLines();
 
+	/// <summary>
+	/// 指定行数の中の総行数を返す
+	/// </summary>
+	/// <param name="line">行数</param>
+	/// <returns>指定行数の中の総行数</returns>
+	int GetColumns(int line);
 
-	//指定した位置のデータを文字列で取得
-	//引数：x,y　取得したい位置
-	//戻値：取得した文字列
-	std::string GetString(DWORD x, DWORD y);
+	/// <summary>
+	/// 指定した(行数,->の中の行数)の文字列を返す
+	/// </summary>
+	/// <param name="line">行数</param>
+	/// <param name="column">行数,->の中の行数</param>
+	/// <returns>行数,->の中の行数の文字列</returns>
+	std::string GetString(int line, int column);
 
-	//指定した位置のデータを整数で取得
-	//引数：x,y　取得したい位置
-	//戻値：取得した値
-	int GetValue(DWORD x, DWORD y);
+	/// <summary>
+	/// 指定した(行数,->の中の行数)の整数を返す
+	/// </summary>
+	/// <param name="line">行数</param>
+	/// <param name="column">行数,->の中の行数</param>
+	/// <returns>行数,->の中の行数の整数</returns>
+	int GetInt(int line, int column);
 
-	//ファイルの列数を取得
-	//引数：なし
-	//戻値：列数
-	size_t GetWidth();
+	/// <summary>
+	/// 指定した(行数,->の中の行数)の浮動小数点を返す
+	/// </summary>
+	/// <param name="line">行数</param>
+	/// <param name="column">行数,->の中の行数</param>
+	/// <returns>行数,->の中の行数の浮動小数点</returns>
+	float GetFloat(int line, int column);
 
-	//ファイルの行数を取得
-	//引数：なし
-	//戻値：行数
-	size_t GetHeight();
 };
-
